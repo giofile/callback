91f5cafd1b957125a4a3c4d734cea34fdbff7182
Giovanni Monsanto <monsanto.g@chello.nl>
2021-08-17 13:30:14 +0200

    calback

---
+++ script.js
@@ -0,0 +1,16 @@
+const huiswerkMaken = ()=>{
+    setTimeout( ()=>{
+        console.log(`Ok, ok, ik ga nu mijn Engels huiswerk maken`);
+
+    }, 1000)
+
+}
+
+
+const klaarMetHuiswerk = () =>{
+
+    console.log( "Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+}
+
+
+huiswerkMaken("wiskunde", klaarMetHuiswerk)
